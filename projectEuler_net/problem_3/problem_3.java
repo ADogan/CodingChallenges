a34ddc6b4481d71dbb74a1b6ed470140623372b3
Ali Dogan <alidogan.just.for.commits@gmail.com>
2017-12-25 00:40:25 +0100

    Remove problem text.

--- projectEuler_net/problem_3/problem_3.java
+++ projectEuler_net/problem_3/problem_3.java
@@ -1,11 +1,6 @@
 
 import java.util.ArrayList;
 
-/*
-The prime factors of 13195 are 5, 7, 13 and 29.
-
-What is the largest prime factor of the number 600851475143 ?
-*/
 public class problem_3{
 
   static long value = 600851475143L;
